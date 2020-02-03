a540e9b8324982bc85e6c5ec7800e6f36ac8a1b6
phamtrananh1088 <phamtrananh1088@gmail.com>
2020-02-03 14:47:54 +0900

    master: mazii searchExample

--- 音声/mazii.Designer.cs
+++ 音声/mazii.Designer.cs
@@ -38,33 +38,35 @@
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnkanji = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Location = new System.Drawing.Point(2, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 12);
-            this.label1.TabIndex = 8;
+            this.label1.TabIndex = 2;
             this.label1.Text = "Sheet name：";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // cbSheetName
             // 
             this.cbSheetName.FormattingEnabled = true;
-            this.cbSheetName.Location = new System.Drawing.Point(87, 31);
+            this.cbSheetName.Location = new System.Drawing.Point(80, 48);
             this.cbSheetName.Name = "cbSheetName";
             this.cbSheetName.Size = new System.Drawing.Size(121, 20);
-            this.cbSheetName.TabIndex = 9;
+            this.cbSheetName.TabIndex = 3;
             // 
             // btntokenizer
             // 
-            this.btntokenizer.Location = new System.Drawing.Point(87, 57);
+            this.btntokenizer.Location = new System.Drawing.Point(80, 74);
             this.btntokenizer.Name = "btntokenizer";
             this.btntokenizer.Size = new System.Drawing.Size(75, 23);
-            this.btntokenizer.TabIndex = 10;
+            this.btntokenizer.TabIndex = 4;
             this.btntokenizer.Text = "tokenizer";
             this.btntokenizer.UseVisualStyleBackColor = true;
             this.btntokenizer.Click += new System.EventHandler(this.btntokenizer_Click);
@@ -72,10 +74,10 @@
             // linkFileName
             // 
             this.linkFileName.AutoSize = true;
-            this.linkFileName.Location = new System.Drawing.Point(19, 6);
+            this.linkFileName.Location = new System.Drawing.Point(12, 23);
             this.linkFileName.Name = "linkFileName";
             this.linkFileName.Size = new System.Drawing.Size(61, 12);
-            this.linkFileName.TabIndex = 6;
+            this.linkFileName.TabIndex = 0;
             this.linkFileName.TabStop = true;
             this.linkFileName.Text = "File name：";
             this.linkFileName.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -83,14 +85,15 @@
             // 
             // txtExcelName
             // 
-            this.txtExcelName.Location = new System.Drawing.Point(87, 6);
+            this.txtExcelName.Location = new System.Drawing.Point(80, 23);
             this.txtExcelName.Name = "txtExcelName";
             this.txtExcelName.ReadOnly = true;
             this.txtExcelName.Size = new System.Drawing.Size(359, 19);
-            this.txtExcelName.TabIndex = 7;
+            this.txtExcelName.TabIndex = 1;
             // 
             // toolStripProgressBar1
             // 
+            this.toolStripProgressBar1.AutoToolTip = true;
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
             this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
             this.toolStripProgressBar1.Step = 1;
@@ -104,7 +107,7 @@
             this.statusStrip1.Location = new System.Drawing.Point(0, 239);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(502, 22);
-            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.TabIndex = 7;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
@@ -121,6 +124,26 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // btnkanji
+            // 
+            this.btnkanji.Location = new System.Drawing.Point(80, 103);
+            this.btnkanji.Name = "btnkanji";
+            this.btnkanji.Size = new System.Drawing.Size(75, 23);
+            this.btnkanji.TabIndex = 5;
+            this.btnkanji.Text = "kanji";
+            this.btnkanji.UseVisualStyleBackColor = true;
+            this.btnkanji.Click += new System.EventHandler(this.btnkanji_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "search -> example";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnsearchExample_Click);
+            // 
             // mazii
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -128,12 +151,15 @@
             this.ClientSize = new System.Drawing.Size(502, 261);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbSheetName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnkanji);
             this.Controls.Add(this.btntokenizer);
             this.Controls.Add(this.linkFileName);
             this.Controls.Add(this.txtExcelName);
             this.Controls.Add(this.statusStrip1);
             this.Name = "mazii";
             this.Text = "mazii";
+            this.Load += new System.EventHandler(this.mazii_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -153,6 +179,8 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnkanji;
+        private System.Windows.Forms.Button button1;
 	}
 }
 
--- 音声/mazii.cs
+++ 音声/mazii.cs
@@ -17,18 +17,97 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.Net;
 using System.Net.Http.Headers;
+using Anh.Translate;
 
 namespace Anh.音声
 {
 	public partial class mazii : Form
 	{
 		private bool _widthChange;
+        private ActionF1 translate_a;
         public mazii()
 		{
 			InitializeComponent();
 		}
 
-        private async void btntokenizer_Click(object sender, EventArgs e)
+        private async void btnsearchExample_Click(object sender, EventArgs e)
+        {
+            if (txtExcelName.Text.Length == 0)
+            {
+                linkFileName.Focus();
+                return;
+            }
+            if (cbSheetName.SelectedItem == null)
+            {
+                cbSheetName.Focus();
+                return;
+            }
+            try
+            {
+
+                string sFileNameTarget = txtExcelName.Text;
+                IWorkbook xlBookTarget = SpreadsheetGear.Factory.GetWorkbook(sFileNameTarget);
+                string xlXheetNm = cbSheetName.SelectedItem.ToString();
+                IWorksheet xlSheet = xlBookTarget.Worksheets[xlXheetNm];
+                IRange rMax = xlSheet.UsedRange;
+                int max = rMax.Rows.RowCount;
+                bool res = false;
+                for (int j = 0; j < rMax.Rows.RowCount; j++)
+                {
+                    string v1 = rMax.Cells[j, 0].Text.Trim();
+                    string v2 = rMax.Cells[j, 1].Text.Trim();
+                    if (v2.Length == 0)
+                    {
+                        toolStripProgressBar1.Value = j*100 / max;
+                        JObject dataEx = await searchExample(v1);
+                        if (dataEx["results"]!=null)
+                        {
+                            for (int i = 0; i < 5; i++)
+                            {
+                                if ( dataEx["results"][i] !=null && dataEx["results"][i]["content"]!=null && dataEx["results"][i]["transcription"]!=null && dataEx["results"][i]["mean"]!=null)
+                                {
+                                    rMax.Cells[j, 1].Value = dataEx["results"][i]["content"].ToString();
+                                    rMax.Cells[j, 2].Value = dataEx["results"][i]["transcription"].ToString();
+                                    rMax.Cells[j, 3].Value = dataEx["results"][i]["mean"].ToString();
+                                    break;
+                                }
+                            }
+                        }
+                        JObject dataExV = await searchExample(v1, "javi");
+                        if (dataExV["results"]!=null)
+                        {
+                            for (int i = 0; i < 5; i++)
+                            {
+                                if (dataExV["results"][i] != null && dataExV["results"][i]["content"] != null && dataExV["results"][i]["transcription"] != null && dataExV["results"][i]["mean"] != null)
+                                {
+                                    rMax.Cells[j, 4].Value = dataExV["results"][i]["content"].ToString();
+                                    rMax.Cells[j, 5].Value = dataExV["results"][i]["transcription"].ToString();
+                                    rMax.Cells[j, 6].Value = dataExV["results"][i]["mean"].ToString();
+                                    break;
+                                }
+                            }
+                        }
+                        res = true;
+                    }
+                }
+
+                if (res)
+                {
+                    xlBookTarget.SaveAs(sFileNameTarget, FileFormat.Excel8);
+                    MessageBox.Show("Updated");
+                }
+                else
+                {
+                    MessageBox.Show("No thing Updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+		private async void btnkanji_Click(object sender, EventArgs e)
 		{
 			if (txtExcelName.Text.Length == 0)
 			{
@@ -46,12 +125,79 @@ namespace Anh.音声
 				string sFileNameTarget = txtExcelName.Text;
 				IWorkbook xlBookTarget = SpreadsheetGear.Factory.GetWorkbook(sFileNameTarget);
 				string xlXheetNm = cbSheetName.SelectedItem.ToString();
-				bool res = await ConvertSheet(xlBookTarget.Worksheets[xlXheetNm]);
+				IWorksheet xlSheet = xlBookTarget.Worksheets[xlXheetNm];
+				IRange rMax = xlSheet.UsedRange;
+                int max = rMax.Rows.RowCount;
+				bool res = false;
+				for (int j = 0; j < rMax.Rows.RowCount; j++)
+				{
+					string v1 = rMax.Cells[j, 0].Text.Trim();
+					string v2 = rMax.Cells[j, 1].Text.Trim();
+					if (v2.Length==0)
+					{
+                        toolStripProgressBar1.Value = j / max * 100;
+						rMax.Cells[j, 1].Value = await kanji(v1);
+						res = true;
+					}
+				}
+			
+				if (res)
+				{
+                    xlBookTarget.SaveAs(sFileNameTarget, FileFormat.OpenXMLWorkbook);
+					MessageBox.Show("Updated");
+				}
+				else
+				{
+					MessageBox.Show("No thing Updated");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+        private async void btntokenizer_Click(object sender, EventArgs e)
+		{
+			if (txtExcelName.Text.Length == 0)
+			{
+				linkFileName.Focus();
+				return;
+			}
+			if (cbSheetName.SelectedItem == null)
+			{
+				cbSheetName.Focus();
+				return;
+			}
+			try
+			{
+				string sFileNameTarget = txtExcelName.Text;
+				IWorkbook xlBookTarget = SpreadsheetGear.Factory.GetWorkbook(sFileNameTarget);
+				string xlXheetNm = cbSheetName.SelectedItem.ToString();
+                IWorksheet xlSheet = xlBookTarget.Worksheets[xlXheetNm];
+                IRange rMax = xlSheet.UsedRange;
+                int max = rMax.Rows.RowCount;
+                bool res = false;
+                for (int j = 0; j < rMax.Rows.RowCount; j++)
+                {
+                    string v1 = rMax.Cells[j, 0].Text.Trim();
+                    string v2 = rMax.Cells[j, 1].Text.Trim();
+                    if (v2.Length == 0)
+                    {
+                        toolStripProgressBar1.Value = j / max * 100;
+                        rMax.Cells[j, 1].Value = await tokenizer(v1);
+                        res = true;
+                    }
+                }
 				if (res)
 				{
                     xlBookTarget.SaveAs(sFileNameTarget, FileFormat.OpenXMLWorkbook);
 					MessageBox.Show("Finished");
-				}
+                }
+                else
+                {
+                    MessageBox.Show("No thing Updated");
+                }
 			}
 			catch (Exception ex)
 			{
@@ -61,28 +207,7 @@ namespace Anh.音声
 		}
 
         #region "tokenizer"
-        private async Task<bool> ConvertSheet(IWorksheet xlSheet)
-		{
-            IRange rMax = xlSheet.UsedRange;
-            for (int j = 0; j < rMax.Rows.RowCount; j++)
-            {
-                string v1 = rMax.Cells[j, 0].Text.Trim();
-                string v2 = rMax.Cells[j, 1].Text.Trim();
-                if (v2.Length==0)
-                {
-                    rMax.Cells[j, 1].Value = await tokenizer(v1);
-                }
-            }
-			return true;
-		}
-
-        /// <summary>
-        /// Voice text
-        /// </summary>
-        /// <param name="q"></param>
-        /// <param name="tl"></param>
-        /// <returns></returns>
-        public async Task<string> tokenizer(string q)
+        private async Task<string> tokenizer(string q)
         {
             string res = null;
             HttpResponseMessage response = null;
@@ -94,7 +219,6 @@ namespace Anh.音声
                     string path = "/api/tokenizer";
                     JObject mixParam = new JObject();
                     mixParam.Add("text", q);
-                    string send = Newtonsoft.Json.JsonConvert.SerializeObject(mixParam);
                     response = await client.PostAsJsonAsync(path, mixParam);
                 }
             }
@@ -111,30 +235,103 @@ namespace Anh.音声
             }
             return res;
         }
-
-        private void Authen(ref HttpRequestHeaders reqH)
+		#endregion
+		
+		#region "kanji"
+		private async Task<string> kanji(string q)
         {
-            reqH.Add("authority", "mazii.net");
-            //reqH.Add("accept", "*/*");
-            //reqH.Accept.Clear();
-            //reqH.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //reqH.Add("scheme", "https");
-            //reqH.Add("method", "GET");
-            //reqH.Add("accept-encoding", "gzip, deflate, br");
-            //reqH.Add("accept-language", "en-US,en;q=0.9,vi;q=0.8,fr-FR;q=0.7,fr;q=0.6");
-            //reqH.Add("referer", "https://translate.google.com.vn/?hl=vi");
-            string chrome = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36";
-            //string edge = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362";
-            reqH.Add("user-agent", chrome);
-            //reqH.GetCookies().Add(new CookieHeaderValue("NID", "188=XFYB2Lnp4nax9yrsHueGO_c5X5mbBYLo5L95ZRPwDB-dvpyttWrVYyabPHPoHv_ItYhmdUOwlR1vE3lFvG7BgzEMxNgjjFX6Uv9gHmz6bvK-IdzbeNeq3oz8b60ZMSEahO97uW3ws2kIzvHQRgR9l_Dl9afmTtzRr3q2IIRWJlU"));
-            //reqH.Add("Cache-Control", new string[] { "no-cache, no-store, must-revalidate" });
-            //reqH.Add("Pragma","no-cache");
-            //reqH.Add("x-client-data", "CIq2yQEIo7bJAQjBtskBCNG3yQEIqZ3KAQioo8oBCLGnygEI4qjKAQigqcoBCPGpygEIl63KAQjNrcoB");
-            //reqH.Add("x-client-data", "aaaCIq2yQEIo7bJAQjBtskBCNG3yQEIqZ3KAQioo8oBCLGnygEI4qjKAQigqcoBCPGpygEIl63KAQjNrcoB");
+            string res = null;
+            HttpResponseMessage response = null;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://data.mazii.net/");
+                    string path = "/kanji/" + Functions.KanjiKodo(q) + ".svg";
+                    JObject mixParam = new JObject();
+                    response = await client.GetAsync(path);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail("エラー: {0}", ex.ToString());
+            }
+            finally
+            {
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                res = await response.Content.ReadAsStringAsync();
+            }
+            return res;
         }
-
 		#endregion
 
+        #region "search"
+        private async Task<JObject> searchExample(string q, string dict = "jaen")
+        {
+            JObject res = null;
+            HttpResponseMessage response = null;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://mazii.net/");
+                    string path = "/api/search";
+                    JObject mixParam = new JObject();
+                    mixParam.Add("dict", dict);
+                    mixParam.Add("type", "example");
+                    mixParam.Add("query", q);
+                    response = await client.PostAsJsonAsync(path, mixParam);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail("エラー: {0}", ex.ToString());
+            }
+            finally
+            {
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                res = await response.Content.ReadAsAsync <JObject>();
+            }
+            return res;
+        }
+        private async Task<JObject> searchKanji(string q)
+        {
+            JObject res = null;
+            HttpResponseMessage response = null;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://mazii.net/");
+                    string path = "/api/search";
+                    JObject mixParam = new JObject();
+                    mixParam.Add("dict", "jaen");
+                    mixParam.Add("type", "kanji");
+                    mixParam.Add("query", q);
+                    mixParam.Add("page", 1);
+                    response = await client.PostAsJsonAsync(path, mixParam);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail("エラー: {0}", ex.ToString());
+            }
+            finally
+            {
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                res = await response.Content.ReadAsAsync<JObject>();
+            }
+            return res;
+        }
+        #endregion
+
+		#region "common"
 		private void lnkFileName_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
 			openFileDialog1.Filter = "xls files (*.xls)|*.xls|xlsx files (*.xlsx)|*.xlsx";
@@ -186,5 +383,25 @@ namespace Anh.音声
 			}
 			senderComboBox.DropDownWidth = width;
 		}
+		#endregion
+
+        private void mazii_Load(object sender, EventArgs e)
+        {
+            translate_a = new ActionF1();
+        }
+	}
+	
+	public static partial class Functions
+	{
+        public static string KanjiKodo(string q)
+        {
+            byte[] ba = UnicodeEncoding.Unicode.GetBytes(q);
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            for (int i = ba.Length - 1; i >= 0; i--)
+            {
+                hex.AppendFormat("{0:x2}", ba[i]);
+            }
+            return hex.ToString();
+        }
 	}
 }
